6fb7ee3bbe7ecd7baeb7125d32e5449ca2635bd7
casey crocamo <casey.crocamo@live.com>
2018-01-28 18:58:56 -0800

    -First commit -added code to connect to hue bridge and blink the lights once -added test CLI

---
+++ HueMusic/Application/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Synesthesia");
+            Console.WriteLine("Connecting to Hue Bridge...");
+            //todo connect with appKey after it is generated
+            HueService.HueService lights = new HueService.HueService();
+            //todo persist appkey in config file
+            string appKey = lights.RegisterApplication("Synesthesia", "CaseyPC").Result;
+            
+            lights.InitializeClient(appKey);
+            lights.BlinkOnce();
+            Console.WriteLine("Connected!");
+        }
+    }
+}
---
+++ HueMusic/HueService/HueService.cs
@@ -0,0 +1,49 @@
+﻿using Q42.HueApi;
+using Q42.HueApi.Interfaces;
+using Q42.HueApi.Models.Bridge;
+using Q42.HueApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace HueService
+{
+    public class HueService
+    {
+        public IBridgeLocator locator = new HttpBridgeLocator();
+
+
+        public ILocalHueClient client;
+
+        public async Task<IEnumerable<LocatedBridge>> FindBridges()
+        {
+             return await locator.LocateBridgesAsync(TimeSpan.FromSeconds(5));
+        }
+
+        public async Task<string> RegisterApplication(string applicationName, string deviceName)
+        {
+            IEnumerable<LocatedBridge> bridges = await FindBridges();
+            client = new LocalHueClient(bridges.First().IpAddress);
+            Console.WriteLine("Please press the button on your Hue Bridge");
+            System.Threading.Thread.Sleep(5000);
+            return await client.RegisterAsync(applicationName, deviceName);
+
+        }
+
+        public async void InitializeClient(string appKey)
+        {
+            IEnumerable<LocatedBridge> bridges = await FindBridges();
+            client = new LocalHueClient(bridges.First().IpAddress);
+            client.Initialize(appKey);
+        }
+
+        public void BlinkOnce()
+        {
+            var command = new LightCommand();
+            command.On = true;
+            command.Alert = Alert.Once;
+            client.SendCommandAsync(command);
+        }
+    }
+}
